ac626388ec65f64c4e56860d654a5e11ad1345c5
Nikolay <tifilum74@mail.ru>
2021-10-05 23:17:02 +0300

    remove .gitignore

---
+++ vendor/laravel/ui/auth-backend/RegistersUsers.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Illuminate\Foundation\Auth;
+
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+
+trait RegistersUsers
+{
+    use RedirectsUsers;
+
+    /**
+     * Show the application registration form.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function showRegistrationForm()
+    {
+
+        if (Auth::user()->role === 'Chief') {
+            return view('auth.register');
+        }
+
+        return Redirect::home();
+    }
+
+    /**
+     * Handle a registration request for the application.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    public function register(Request $request)
+    {
+        if (Auth::user()->role === 'Chief') {
+            $this->validator($request->all())->validate();
+
+            event(new Registered($user = $this->create($request->all())));
+
+            if ($response = $this->registered($request, $user)) {
+                return $response;
+            }
+
+            return $request->wantsJson()
+                ? new JsonResponse([], 201)
+                : redirect($this->redirectPath());
+        }
+        return Redirect::home();
+    }
+
+    /**
+     * Get the guard to be used during registration.
+     *
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected function guard()
+    {
+        return Auth::guard();
+    }
+
+    /**
+     * The user has been registered.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param mixed $user
+     * @return mixed
+     */
+    protected function registered(Request $request, $user)
+    {
+        //
+    }
+
+    /**
+     * @return bool
+     */
+    public function isRoleCoordinator(): bool
+    {
+        if (Auth::user() && Auth::user()->role === 'Coordinator' && Auth::user()->role === 'Chief') {
+            return true;
+        }
+        return false;
+    }
+}
